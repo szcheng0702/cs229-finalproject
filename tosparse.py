26e887b0b674724a7d9bf01dd3ce61fa189eb306
szcheng0702 <szcheng0702@gmail.com>
2017-11-16 13:13:02 -0800

    add comments

--- tosparse.py
+++ tosparse.py
@@ -3,8 +3,13 @@ import scipy.sparse
 
 #output is a list of lists of tuples
 #The largest list is called features
+
+##
 features=[[(0,1),(1,9)],[(1,2),(2,1)]]
 dictsize=3 #size of the dictionary, i.e.how many words in total
+##
+#These are test data, will be replaced by the actual data later
+
 for i in range(len(features)):#i is the index of the article
     numwords=len(features[i])
     currenti_id=i*np.ones(numwords)
